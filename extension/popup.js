c8de1e2d591de3a0439f939c4a0bc160f2447c08
cheevu-RS <ramrsrs99@gmail.com>
2018-11-26 21:19:14 +0530

    removed python, added OMDB

--- extension/popup.js
+++ extension/popup.js
@@ -9,15 +9,40 @@ async function get_url(callback){
 
 function postData(input) {
         console.log(input);
-        var xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function() {
+        //to obtain movie name,year
+        var scrape = new XMLHttpRequest();
+        scrape.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            let dummy = $(this.responseText);
+            var name = $(dummy).find('img.logo')[0]['alt'];
+            //Netflix year is last updates year of movie, not what OMDB has.
+            //var year = $('.year',dummy).text();
+            url = "http://www.omdbapi.com/?apikey=9429ae3b&t="+name;//+"&y="+year;
+            console.log(url);
+            //to get details from OMDB api
+            var xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function() {
             if (this.readyState == 4 && this.status == 200) {
-            //alert(this.responseText);
-            //console.log(this.responseText);
-            document.getElementById('status').innerHTML=this.responseText;        
-           }
+                //alert(this.responseText);
+                stuff = JSON.parse(this.responseText);
+                Object.keys(stuff).forEach(function (key) {
+                    if(key==='Ratings')
+                    {
+                        if(stuff[key]!==[])
+                        {
+                    document.getElementById('status').innerHTML+="<b>"+key+"</b>"+":"+stuff[key]+"<br>"; 
+                        }
+                    }
+                    document.getElementById('status').innerHTML+="<b>"+key+"</b>"+":"+stuff[key]+"<br>"; 
+                 });
+                //document.getElementById('status').innerHTML=this.responseText;        
+                }
+            };
+        xhttp.open("GET", url, true);
+        xhttp.send(); 
+        }
         };
-        xhttp.open("POST", "http://127.0.0.1:5000/", true);
-        xhttp.send(input); 
+        scrape.open("GET", input, true);
+        scrape.send();
     }
 get_url(postData);
