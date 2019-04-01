34f89da1c0e7fe8be7932eb9dc70ab01ceed6587
cheevu-RS <ramrsrs99@gmail.com>
2019-04-01 11:30:07 +0530

    fixed bugs

--- extension/popup.js
+++ extension/popup.js
@@ -1,43 +1,42 @@
-async function get_url(callback){
-    tabs =  await chrome.tabs
-    tabs.query({'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT},
-    function(tabs){
-    callback(tabs[0].url)
-    }
-);
+async function get_url(callback) {
+    tabs = await chrome.tabs;
+    tabs.query({
+            'active': true,
+            'windowId': chrome.windows.WINDOW_ID_CURRENT
+        },
+        function (tabs) {
+            callback(tabs[0].url)
+        }
+    );
 };
 
 function postData(input) {
-        console.log(input);
-        //to obtain movie name,year
-        var scrape = new XMLHttpRequest();
-        scrape.onreadystatechange = function() {
+    //to obtain movie name,year
+    var scrape = new XMLHttpRequest();
+    scrape.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             let dummy = $(this.responseText);
-            var name = $(dummy).find('.text.image-fallback-text').text();
+            let name = $(dummy).find('img.logo').attr('alt');
             console.log(name);
             //Netflix year is last updates year of movie, not what OMDB has.
             //var year = $('.year',dummy).text();
-            url = "http://www.omdbapi.com/?apikey=9429ae3b&t="+name;//+"&y="+year;
-            console.log(url);
+            url = "http://www.omdbapi.com/?apikey=9429ae3b&t=" + name; //+"&y="+year;
             //to get details from OMDB api
             var xhttp = new XMLHttpRequest();
-            xhttp.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 200) {
-                //alert(this.responseText);
-                stuff = JSON.parse(this.responseText);
-                Object.keys(stuff).forEach(function (key) {
-                if(key!=="Ratings")
-                    document.getElementById('status').innerHTML+="<b>"+key+"</b>"+":"+stuff[key]+"<br>"; 
-                });
-                //document.getElementById('status').innerHTML=this.responseText;        
+            xhttp.onreadystatechange = function () {
+                if (this.readyState == 4 && this.status == 200) {
+                    stuff = JSON.parse(this.responseText);
+                    Object.keys(stuff).forEach(function (key) {
+                        if (key !== "Ratings")
+                            document.getElementById('status').innerHTML += "<b>" + key + "</b>" + ":" + stuff[key] + "<br>";
+                    });
                 }
             };
-        xhttp.open("GET", url, true);
-        xhttp.send(); 
+            xhttp.open("GET", url, true);
+            xhttp.send();
         }
-        };
-        scrape.open("GET", input, true);
-        scrape.send();
-    }
+    };
+    scrape.open("GET", input, true);
+    scrape.send();
+}
 get_url(postData);
